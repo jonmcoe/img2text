97e5f767b741430e0ad971cb8ed4aa1ba69d548b
Jon Coe <jon@seatgeek.com>
2018-10-20 10:38:57 -0400

    add comments and fix off by one

--- img2text.py
+++ img2text.py
@@ -4,18 +4,19 @@ import numpy as np
 from PIL import Image
 
 
-GRAY_CHARS = [' ', '.', ':', '|', '+', '=', '3', '#', '@']  # length must be power of 2
-BOUNDS = list(range(0, 2**8, 2**8 // len(GRAY_CHARS)))
+GRAY_CHARS = [' ', '.', ':', '|', '+', '=', '3', '#', '@']  # length must be power of 2 + 1
+BOUNDS = list(range(0, 2**8, 2**8 // (len(GRAY_CHARS) - 1)))
 
 
 def main():
     input_img_filename = sys.argv[1]
     pixel_scale = int(sys.argv[2]) if len(sys.argv) > 2 else 1
     g = (
-        (GRAY_CHARS[p - 1] for p in r)
+        (GRAY_CHARS[p - 1] for p in r)  # TODO: consider pixel scale and average the rectangle here
         for r in np.digitize(np.array(Image.open(input_img_filename).convert(mode='L')), BOUNDS)
     )
-    for l in list(g)[::pixel_scale]:  # TODO: how to do this smart with iterator and not list cast?
+    # TODO: how to do this smart with iterator and not list cast? (or do above instead, then no need)
+    for l in list(g)[::pixel_scale]:
         print(''.join(list(l)[::pixel_scale]))
 
 
