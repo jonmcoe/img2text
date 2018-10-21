9d30c23191564a84850babb4e1d53a8953a0f0f4
Jon Coe <jon@seatgeek.com>
2018-10-21 00:40:50 -0400

    perform skip/slice the numpy array

--- img2text.py
+++ img2text.py
@@ -11,12 +11,9 @@ BOUNDS = list(range(0, 2**8, 2**8 // (len(GRAY_CHARS) - 1)))
 def main():
     input_img_filename = sys.argv[1]
     pixel_scale = int(sys.argv[2]) if len(sys.argv) > 2 else 1
-    g = (
-        (GRAY_CHARS[p - 1] for p in r)  # TODO: consider pixel scale and average the rectangle here
-        for r in np.digitize(np.array(Image.open(input_img_filename).convert(mode='L')), BOUNDS)
-    )
-    # TODO: how to do this smart with iterator and not list cast? (or do above instead, then no need)
-    sys.stdout.writelines(''.join(list(r)[::pixel_scale]) + '\n' for r in list(g)[::pixel_scale])
+    img_matrix = np.digitize(np.array(Image.open(input_img_filename).convert(mode='L')), BOUNDS)[::pixel_scale, ::pixel_scale]
+    g = ((GRAY_CHARS[p - 1] for p in row) for row in img_matrix)
+    sys.stdout.writelines(''.join(r) + '\n' for r in g)
     sys.stdout.flush()
 
 
