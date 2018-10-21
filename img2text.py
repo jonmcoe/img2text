1177c32fc0ae2290abac83ae9c76c777708d69e3
Jon Coe <jon@seatgeek.com>
2018-10-21 01:14:51 -0400

    use awesome numpy indexing

--- img2text.py
+++ img2text.py
@@ -4,16 +4,24 @@ import numpy as np
 from PIL import Image
 
 
-GRAY_CHARS = [' ', '.', ':', '|', '+', '=', '3', '#', '@']  # length must be power of 2 + 1
+GRAY_CHARS = np.array([' ', '.', ':', '|', '+', '=', '3', '#', '@'])  # length must be power of 2 + 1
 BOUNDS = list(range(0, 2**8, 2**8 // (len(GRAY_CHARS) - 1)))
+
+
+def make_text_from_image(input_img_filename, pixel_scale):
+    return GRAY_CHARS[
+        np.digitize(np.array(Image.open(input_img_filename).convert(mode='L')), BOUNDS)[::pixel_scale, ::pixel_scale]
+    ]
 
 
 def main():
     input_img_filename = sys.argv[1]
     pixel_scale = int(sys.argv[2]) if len(sys.argv) > 2 else 1
-    img_matrix = np.digitize(np.array(Image.open(input_img_filename).convert(mode='L')), BOUNDS)[::pixel_scale, ::pixel_scale]
-    g = ((GRAY_CHARS[p - 1] for p in row) for row in img_matrix)
-    sys.stdout.writelines(''.join(r) + '\n' for r in g)
+    sys.stdout.writelines(
+        ''.join(r) + '\n'
+        for r in
+        make_text_from_image(input_img_filename, pixel_scale)
+    )
     sys.stdout.flush()
 
 
